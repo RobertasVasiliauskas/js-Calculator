4d76b2ec7b20a9faa5772596c7da6623c7766191
Robertas_Vas <105080418+robertasvasiliauskas@users.noreply.github.com>
2023-11-02 01:12:53 +0200

    bug fix

--- calculator.js
+++ calculator.js
@@ -1,29 +1,42 @@
 const displayFirstRow = document.querySelector("#firstRow");
 const displaySecondRow = document.querySelector("#secondRow");
-const buttonsNumbers = document.querySelectorAll(".numbers")
-const buttonMath = document.querySelectorAll(".math")
-const buttonClear = document.querySelector(".controls-btnClear")
-const buttonDelete = document.querySelector(".controls-btnDelete")
-let text
+const buttonsNumbers = document.querySelectorAll(".numbers");
+const buttonMath = document.querySelectorAll(".math");
+const buttonClear = document.querySelector(".controls-btnClear");
+const buttonDelete = document.querySelector(".controls-btnDelete");
+const buttonFloat = document.querySelector("#dot");
+let text;
+let count = 0;
 
 function initialSetup() {
     displayFirstRow.textContent = "0";
 }
 
-buttonClear.addEventListener('click', () =>{
+buttonClear.addEventListener('click', () => {
     displayFirstRow.textContent = "0";
     displaySecondRow.textContent = "";
-    text = 0;
+    count = 0;
 })
 
-buttonDelete.addEventListener('click', () =>{
+buttonFloat.addEventListener('click', () => {
+    if (count === 0) {
+        let newNumber = DomToText(displayFirstRow.outerHTML) + ".0";
+        newNumber = parseFloat(newNumber).toFixed(1);
+        newNumber = newNumber.slice(0, newNumber.length - 1);
+        displayFirstRow.textContent = newNumber.toString();
+        count = count + 1;
+    }
+})
+
+buttonDelete.addEventListener('click', () => {
 
     let string = DomToText(displayFirstRow.innerHTML);
 
-    string = string.slice(0, string.length-1);
-    text = string;
+    string = string.slice(0, string.length - 1);
 
-    string === "" ?  displayFirstRow.textContent = "0" :  displayFirstRow.textContent = string;
+    string === "" ? displayFirstRow.textContent = "0" : displayFirstRow.textContent = string;
+    if (string === "") count = 0;
+
 })
 
 function DomToText(string) {
@@ -35,8 +48,13 @@ function DomToText(string) {
         for (let j = 0; j < numbers.length; j++) {
             if (string[i].toString() === numbers[j].toString() && start === -1) start = i;
 
-            if (string[i].toString() === numbers[j].toString()) end = i + 1;
+            if (string[i].toString() === numbers[j].toString() || string[i].toString() === ".") end = i + 1;
         }
+    }
+
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === ".") return string.slice(start, end).concat(0)
+
     }
     return string.slice(start, end);
 }
@@ -67,8 +85,6 @@ buttonsNumbers.forEach((item) => {
             displayFirstRow.textContent += item.id;
             text = displayFirstRow.textContent;
         } else alert("Max length is 21");
-
-
     })
 })
 
@@ -91,49 +107,60 @@ function Math(numberOne, numberTwo, operator) {
 
 buttonMath.forEach((item) => {
     item.addEventListener('click', () => {
-
+        let first = document.querySelector("#firstRow").outerHTML;
+        let second = document.querySelector("#secondRow").outerHTML;
         let result;
+
 
         switch (item.id) {
             case "*":
-                text = DomToText(displayFirstRow.textContent);
-                displaySecondRow.textContent = text + " *";
-                displayFirstRow.textContent = "0";
+                if (displaySecondRow.textContent !== "") {
+                    displaySecondRow.textContent += " *"
+                } else {
+                    text = DomToText(displayFirstRow.textContent);
+                    displaySecondRow.textContent = text + " *";
+                    displayFirstRow.textContent = "0";
+                }
+
                 break;
             case "÷":
-                text = DomToText(displayFirstRow.textContent);
-                displaySecondRow.textContent = text + " ÷";
-                displayFirstRow.textContent = "0";
+                if (displaySecondRow.textContent !== "") {
+                    displaySecondRow.textContent += " ÷"
+                } else {
+                    text = DomToText(displayFirstRow.textContent);
+                    displaySecondRow.textContent = text + " ÷";
+                    displayFirstRow.textContent = "0";
+                }
                 break;
             case "-":
-                text = DomToText(displayFirstRow.textContent);
-                displaySecondRow.textContent = text + " -";
-                displayFirstRow.textContent = "0";
+                if (displaySecondRow.textContent !== "") {
+                    displaySecondRow.textContent += " -"
+                } else {
+                    text = DomToText(displayFirstRow.textContent);
+                    displaySecondRow.textContent = text + " -";
+                    displayFirstRow.textContent = "0";
+                }
                 break;
             case "+":
-                text = DomToText(displayFirstRow.textContent);
-                displaySecondRow.textContent = text + " +";
-                displayFirstRow.textContent = "0";
+                if (displaySecondRow.textContent !== "") {
+                    displaySecondRow.textContent += " +"
+                } else {
+                    text = DomToText(displayFirstRow.textContent);
+                    displaySecondRow.textContent = text + " +";
+                    displayFirstRow.textContent = "0";
+                }
                 break;
             case "=":
-
-                let first = document.querySelector("#firstRow").outerHTML;
-                let second = document.querySelector("#secondRow").outerHTML;
-
                 if (DomToText(second) === "" && DomToText(first) !== "") {
                     result = Math(parseFloat(DomToText(first)), 0, "+");
-                }
-                else if((DomToText(second) !== "" && DomToText(first) !== "" && !FindSymbol(document.querySelector("#secondRow").outerHTML))){
+                } else if ((DomToText(second) !== "" && DomToText(first) !== "" && !FindSymbol(document.querySelector("#secondRow").outerHTML))) {
                     result = Math(parseFloat(DomToText(first)), 0, "+");
-                }
-                else result = Math(parseFloat(DomToText(second)), parseFloat(DomToText(first)), FindSymbol(document.querySelector("#secondRow").outerHTML));
+                } else result = Math(parseFloat(DomToText(second)), parseFloat(DomToText(first)), FindSymbol(document.querySelector("#secondRow").outerHTML));
 
                 displaySecondRow.textContent = result;
                 displayFirstRow.textContent = "0";
                 text = displaySecondRow.textContent;
-                break;
-
-            case ".":
+                count = 0;
                 break;
         }
 
