94ccccc3a7f1581002f9918f38ea48015c2ff76a
Robertas_Vas <105080418+robertasvasiliauskas@users.noreply.github.com>
2023-11-01 21:42:11 +0200

    Added initial setup for calculator
    Now calculator display is showing 0 after reloading page.

---
+++ calculator.js
@@ -0,0 +1,13 @@
+
+const displayFirstRow = document.querySelector("#firstRow");
+const displaySecondRow = document.querySelector("#secondRow");
+const buttonsNumbers = document.querySelectorAll(".numbers")
+const buttonMath = document.querySelectorAll(".Math")
+
+function initialSetup() {
+    displayFirstRow.textContent = "0"
+}
+
+
+
+initialSetup();
